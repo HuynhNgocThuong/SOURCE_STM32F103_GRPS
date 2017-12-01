a6703f6896fbe4631d626782f9d06e2acf073d9f
Huỳnh Ngọc Thương <huynhngocthuong0208@gmail.com>
2017-12-01 14:16:15 +0700

    Update main.c

--- main.c
+++ main.c
@@ -18,8 +18,24 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
+/*__________________________________________________-*/
+bool indexOf(char* String, char* Character);
+void SIM_Send(char* cmd);
 void SIM_sendCommand(char* cmd);
-
+char* SIM_replyCommand(int delay);
+void SIM_DeleteArray();
+void SIM_begin();
+//GPRS_Function
+bool SIM_connectGPRS();
+void SIM_disconnectGPRS();
+void SIM_readserverResponse();
+bool SIM_getValue();
+void SIM_getContent();
+//SMS_Function
+void SIM_deleteMessage();
+void SIM_sendSMS();
+void SIM_smsFormat();
+void SIM_checkAccount();
 
 /* Private function prototypes -----------------------------------------------*/
 #ifdef __GNUC__
@@ -30,12 +46,14 @@ void SIM_sendCommand(char* cmd);
 
 PUTCHAR_PROTOTYPE  
 {
-    HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 100);
+    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 100);
     return ch;
 }
 struct dataGPRS_t{
 	char reply[100];
 };
+char SendSMS = 26;
+
 
 struct dataGPRS_t dataGPRS;
 /*Check character in string*/
@@ -51,7 +69,7 @@ bool indexOf(char* String, char* Character){
 /* Sim Send*/
 void SIM_Send(char* cmd){
 	int i = strlen(cmd);
-	HAL_UART_Transmit(&huart1,(uint8_t *) cmd, i, 1000);
+	HAL_UART_Transmit(&huart2,(uint8_t *) cmd, i, 1000);
 }
 /* SIM Send AT Command*/
 void SIM_sendCommand(char* cmd) {
@@ -60,19 +78,20 @@ void SIM_sendCommand(char* cmd) {
 }
 /* Sim Send Reply Command*/
 char* SIM_replyCommand(int delay){
-		HAL_UART_Receive(&huart1, (uint8_t *)dataGPRS.reply, 100, 1000);
+		HAL_UART_Receive(&huart2, (uint8_t *)dataGPRS.reply, 100, 1000);
 		printf("%s\n",dataGPRS.reply);
 		HAL_Delay(delay);
 	return dataGPRS.reply;
 }
 /* Delete Array*/
 void SIM_DeleteArray(){
+/* _____________________________SIM_GPRS_________________________*/
 	int length = strlen(dataGPRS.reply);
 	for(int i = 0; i<length; i++){
 		dataGPRS.reply[i] = 0;
 	}
 }
-
+/* _____________________________SIM_GPRS_________________________*/
 bool SIM_connectGPRS(){
 	printf("1\n");
 	SIM_sendCommand("AT+SAPBR=1,1");
@@ -151,14 +170,34 @@ void SIM_getContent(char* _Parameter){
 //	SIM_sendCommand(_Parameter);
 //	SIM_replyCommand(1000);
 //	SIM_DeleteArray();
-	
-
-	
+}
+bool SIM_getValue(){
+	printf("-------------------- -Start g----------------------\n");
+  char check_connect = 0;
+  while (!SIM_connectGPRS()) { 
+     check_connect++;
+     SIM_reset();
+     SIM_begin();
+      if(check_connect==3){ 
+			SIM_sendSMS(PhoneNumber, "SYNTAX ERROR! \n ID: \""+ (String)measuring_point_id+"\"\n Sim800 connect GPRS fail !! \n check your module:\n ");
+			return false;
+			}
+			
+	String date_of_value = (String)yearr+"-"+(String)mon +"-" +(String)dayy ;
+  Serial.println(date_of_value);
+  String hour_of_value = (String)hh1+":"+(String)mm1 +":" +(String)ss1;;
+  Serial.println(hour_of_value);
+  SIM_getContent( "action=index&measuring_point_id=" + (String)measuring_point_id + "&date="+ date_of_value + "&time="+ hour_of_value+ "&value1=" + (String)temp1 + "&value2=" + (String)temp2 + "&value3=" + (String)temp3 + "&account="+ account + "&battery="+(String)battery);
+  SIM_readserverResponse();
+  SIM_disconnectGPRS();
+  SIM_smsFormat();
+  printf("-------------------- -Get finished---------------------- -");
+return true;
+			
+}
 	
 }
-
-/* Sim function*/
-/* _____________________________SIM_SMS________________________ */
+/* _____________________________SIM_SMS_________________________*/
 void SIM_deleteMessage() {
   SIM_sendCommand("AT+CMGD=1,4"); //delete all message on SIM
   SIM_replyCommand(100);
@@ -177,6 +216,7 @@ void SIM_smsFormat() {
 }
 
 void SIM_sendSMS(char* targetNumber, char* message){
+	
 	printf("|%s|\n|%s|", targetNumber, message);
 //	SIM_Send("AT+CMGS=\"");
 //	SIM_Send(targetNumber);
@@ -188,14 +228,35 @@ void SIM_sendSMS(char* targetNumber, char* message){
 	HAL_Delay(500);
 	SIM_sendCommand(message);
 	HAL_Delay(300);
-	HAL_UART_Transmit(&huart1,(uint8_t *)(0x1A), 1, 1000);
+	HAL_UART_Transmit(&huart2,(uint8_t *)&SendSMS, 1, 1000);
 	HAL_Delay(300);
-	HAL_UART_Transmit(&huart1,(uint8_t *)(0x1A), 1, 1000);
+	HAL_UART_Transmit(&huart2,(uint8_t *)&SendSMS, 1, 1000);
 	printf("5\n");
 	
 }
+/*______________________________SIM_ACCOUNT____________________*/
+void SIM_checkAccount(){
+	printf("Check Account\n");
+	do{
+			SIM_sendCommand("AT+CUSD=1,\"*101#\"");
 
-/* Sim begin*/
+		  if(indexOf(SIM_replyCommand(300),"ERROR")) {
+      printf("-------------------------> Reset Module SIM-----");
+      //SIM_sendCommand("AT+CFUN=1,1"); //Reset luon;
+      SIM_begin();
+    }
+	}while(!(indexOf(SIM_replyCommand(300),"+CUSD")));
+	SIM_replyCommand(300);
+	SIM_DeleteArray();
+	SIM_sendCommand("AT+CUSD=0");
+	
+//	account = getUSSDMessage(data);
+//  SIM_sendCommand("AT+CUSD=0");
+//  SIM_buffer_flush();
+//  delay(100);
+}
+
+/* _____________________________SIM_BEGIN________________________ */
 void SIM_begin(){
 	HAL_Delay(1000);
 	do{
@@ -230,7 +291,6 @@ void SIM_begin(){
 
 
 
-
 int main(void)
 {
   HAL_Init();
@@ -241,17 +301,18 @@ int main(void)
 	
 	SIM_disconnectGPRS();
 	SIM_begin();
-	SIM_sendSMS(PhoneNumber, "Thuongdeptrai");
+  SIM_sendSMS(PhoneNumber, "Thuongdeptrai");
 //	if(SIM_connectGPRS()) {
-//		printf("Dung\n");
-//		SIM_getContent("1605.61665xNx10814.32328xE");
+//	printf("Dung\n");
+//	//	SIM_getContent("1605.61665xNx10814.32328xE");
 //	}
 //	else printf("Sai\n");
 
 	
   while (1)
   {
-
+//	printf("Thuong\r");
+//		HAL_Delay(1000);
 
   }
 
